c91f445100b25f9e6bfc8fafea4ff57010fe5016
grykah <67068320+grykah@users.noreply.github.com>
2022-07-08 09:28:30 -0700

    Implement code to prevent lock

--- PreventLock/PreventLock/PreventLock.py
+++ PreventLock/PreventLock/PreventLock.py
@@ -1,1 +1,30 @@
 
+import pyautogui
+import time
+
+def prevent_lock(button):
+    try:
+        print ('Press CTRL+C to stop.')
+        while True:
+            pyautogui.press(button)     # Key pressed
+            time.sleep(10)
+
+    except Exception as ex:
+        print ('no_lock | Error: ', ex)
+
+def main():
+    try:
+        print ('\nPrevent Windows screenlock')
+        kb_button = str(input('Enter keyboard button: '))
+        
+        print ('\nRunning')
+        prevent_lock(kb_button)
+
+    except KeyboardInterrupt:
+        print('\nStopped')
+
+    except Exception as ex:
+        print ('main | Error: ', ex)
+
+if __name__ == "__main__":
+    main()
